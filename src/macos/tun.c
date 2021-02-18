c94468e44f8dfb3f8dde494faeab296e3536f859
Constantine Theocharis <cthe@mailbox.org>
2021-02-18 15:02:49 +0000

    Refine tun interface

--- src/macos/tun.c
+++ src/macos/tun.c
@@ -1,29 +1,7 @@
 #include "../tun.h"
 #include <stdlib.h>
 
-struct tun
+int tun_open(struct in6_addr *addr, struct in6_addr *mask)
 {
-    char *name;
-};
-
-struct tun *tun_new()
-{
-    struct tun *t = malloc(sizeof(struct tun));
-    t->name = "tun0";
-    return t;
-}
-
-char *tun_name(struct tun const *t)
-{
-    return t->name;
-}
-
-int *tun_recv(struct tun const *t, void *buffer, size_t n)
-{
-    return 0;
-}
-
-int *tun_send(struct tun const *t, void const *buffer, size_t n)
-{
-    return 0;
+    return -1;
 }
--- src/tun.h
+++ src/tun.h
@@ -1,49 +1,13 @@
 #pragma once
+#include <netinet/in.h>
 #include <stddef.h>
 #include <stdint.h>
 
 /**
- * Contains information about the network tunnel.
+ * Allocate a new tunnel bound at the given range of IPs..
+ *
+ * @param addr - The IP address to bind the tunnel to.
+ * @param addr - A mask for the IP address.
+ * @returns - A file descriptor of the allocated tunnel, or negative in case of error.
  */
-struct tun;
-
-/**
- * Allocate a new tunnel.
- *
- * @returns - The allocated tunnel.
- */
-struct tun *tun_new();
-
-/**
- * Get the name of the given tunnel.
- *
- * @param t - The tunnel.
- */
-char *tun_name(struct tun const *t);
-
-/**
- * Receive a packet from the given tunnel.
- *
- * Make sure that the provided buffer is large enough because the packet will
- * be truncated if its size exceeds n.
- *
- * @param t - The tunnel.
- * @param buffer - The buffer to store the received data in.
- * @param n - The size of the buffer, in bytes.
- * @returns - An error code, 0 for success, non-zero for failure.
- *
- * @Todo: better return code.
- */
-int *tun_recv(struct tun const *t, void *buffer, size_t n);
-
-/**
- * Send a packet to the given tunnel.
- *
- * @param t - The tunnel.
- * @param buffer - The buffer to read the packet data from.
- * @param n - The size of the buffer, in bytes.
- * @returns - An error code, 0 for success, non-zero for failure.
- *
- *  @Todo: better return code.
- */
-int *tun_send(struct tun const *t, void const *buffer, size_t n);
+int tun_open(struct in6_addr *addr, struct in6_addr *mask);
