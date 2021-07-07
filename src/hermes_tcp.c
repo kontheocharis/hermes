0d7deb74abcd5f6ce62c0cb2dde5dcdad248cc71
Constantine Theocharis <cthe@mailbox.org>
2021-07-07 15:40:00 +0000

    WIP: Implement tun_open for macos

--- src/hermes_tcp.c
+++ src/hermes_tcp.c
@@ -1,8 +1,27 @@
 #include "../include/hermes_tcp.h"
 #include "tun.h"
+#include "log.h"
+#include <unistd.h>
+#include <ifaddrs.h>
+#include <stdlib.h>
+#include <string.h>
 
 char *herm_get_tun_name()
 {
-    struct tun *t = tun_new();
-    return tun_name(t);
+    int fd = tun_open(NULL, NULL);
+    system("ifconfig utun2 inet 10.0.0.0 10.0.0.0");
+    if (fd == -1) {
+        return "Bad";
+    }
+
+    char buf[4096];
+    while (1) {
+        read(fd, buf, sizeof(buf));
+        log_info("%s", buf);
+        memset(buf, 0, sizeof(buf));
+        sleep(1);
+    }
+
+    close(fd);
+    return "Hello, world!";
 }
--- src/macos/tun.c
+++ src/macos/tun.c
@@ -1,7 +1,60 @@
 #include "../tun.h"
+#include "../log.h"
 #include <stdlib.h>
+#include <net/if_utun.h>
+#include <string.h>
+#include <sys/ioctl.h> // ioctl
+#include <sys/socket.h>
+#include <sys/sys_domain.h>
+#include <sys/kern_control.h> // struct socketaddr_ctl
+#include <unistd.h>
+#include <net/if.h>
+
 
 int tun_open(struct in6_addr *addr, struct in6_addr *mask)
 {
-    return -1;
+    // Open a datagram socket
+    int fd = socket(PF_SYSTEM, SOCK_DGRAM, SYSPROTO_CONTROL);
+    if (fd == -1) {
+        log_error("Failed to open socket");
+        return -1;
+    }
+    log_info("Successfully opened socket with fd=%i", fd);
+
+    // Setup ctl_info for utun
+    struct ctl_info utun_ctl_info;
+    strlcpy(utun_ctl_info.ctl_name, UTUN_CONTROL_NAME, sizeof(utun_ctl_info.ctl_name));
+    if (ioctl(fd, CTLIOCGINFO, &utun_ctl_info) == -1) {
+        close(fd);
+        log_error("Unexpected error from ioctl");
+        return -1;
+    }
+    log_info("utun ctl info: ctl_id=%i, ctl_name=%s", utun_ctl_info.ctl_id, utun_ctl_info.ctl_name);
+
+    // Create a sockaddr_ctl
+    struct sockaddr_ctl sockaddr = {
+        .sc_id = utun_ctl_info.ctl_id,
+        .sc_len = sizeof(struct sockaddr_ctl),
+        .sc_family = AF_SYSTEM,
+        .ss_sysaddr = AF_SYS_CONTROL,
+        .sc_unit = 0, // 0 means dynamically allocated, see https://developer.apple.com/forums/thread/85473.
+    };
+    if (connect(fd, (struct sockaddr *)&sockaddr, sizeof(sockaddr)) == -1) {
+        close(fd);
+        log_error("Could not connect to socket");
+        return -1;
+    }
+    log_info("Successfully connected to socket with fd=%i", fd);
+
+    // Get interface name
+    char ifname[IFNAMSIZ];
+    socklen_t ifname_len = IFNAMSIZ;
+    if (getsockopt(fd, SYSPROTO_CONTROL, UTUN_OPT_IFNAME, ifname, &ifname_len) == -1) {
+        close(fd);
+        log_error("Could get utun interface name");
+        return -1;
+    }
+    log_info("Interface name is %s", ifname);
+
+    return fd;
 }
